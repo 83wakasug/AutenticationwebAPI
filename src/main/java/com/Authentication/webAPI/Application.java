8df1debeccc3d6a8f7d48167461fa5d407e681ec
Wakana <83waksug@gafe.molndal.se>
2023-11-24 08:34:40 +0100

    Working program

--- src/main/java/com/Authentication/webAPI/Application.java
+++ src/main/java/com/Authentication/webAPI/Application.java
@@ -1,13 +1,39 @@
 package com.Authentication.webAPI;
 
+import com.Authentication.webAPI.security.entity.Role;
+import com.Authentication.webAPI.security.entity.Roles;
+import com.Authentication.webAPI.security.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+	}
+
+	@Bean
+	CommandLineRunner run(RoleRepository roleRepository){
+		return args ->{
+
+			if(roleRepository.findByAuthority(Role.ROLE_ADMIN).isPresent()) return;
+			saveRoles(roleRepository,Role.ROLE_ADMIN);
+			saveRoles(roleRepository,Role.ROLE_USER);
+		};
+	}
+
+	public void saveRoles(RoleRepository repository, Role role){
+		Roles roles = new Roles();
+		try {
+			roles.setAuthority(role);
+		}catch (Exception e){
+
+		}
+		repository.save(roles);
 	}
 
 }
